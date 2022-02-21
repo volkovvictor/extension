71b8a95ed072845ce811fa125a5d57a279e48745
Victor Volkov <vityan2504@gmail.com>
2022-02-21 11:13:42 +0300

    add (script.js) did task

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 'use struct';
 
+class First {
+   hello() {
+      console.log('Привет я метод родителя!');
+   }
+}
+
+class Second extends First {
+   constructor() {
+      super();
+   }
+
+   hello() {
+      super.hello();
+      console.log('А я наследуемый метод!');
+   }
+}
+
+const second = new Second();
+
+second.hello();
